29324278e73446991422d10df637dc45b869524b
Tommy <quangtrung54677@gmail.com>
2026-09-22 23:57:37 +0700

    day3 - searchBox

--- src/app/component/product-list/product-list.ts
+++ src/app/component/product-list/product-list.ts
@@ -1,29 +1,42 @@
 import { Component, inject } from '@angular/core';
 import { Product, ProductService } from '../../product.service';
 import { ProductItemComponent } from '../../product-item-component/product-item-component';
-import { it } from 'vitest';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductItemComponent, RouterLink],
+  imports: [ProductItemComponent, RouterLink, FormsModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductList {
-
   //Dependency Injection Service
   //constructor(private productService: ProductService) {}
   // inject giúp DI mà không cần sử dụng constructor để truyền vào
   // inject giúp DI ở cấp độ khai báo biến
   private productService = inject(ProductService);
 
+  searchBox: string = '';
+
   // Declaration Product list
   productList = this.productService.productList;
+  newProductList : Product[] = this.productList();
 
   // Buy Product Func
   buyProduct(product: Product) {
     product.isAvailable = false;
   }
-  protected readonly it = it;
+
+  // Filter Product list
+  searchByKeyword(){
+    let keyWord = this.searchBox.trim().toLowerCase();
+
+    if (keyWord === ""){
+      this.newProductList = this.productList();
+      return;
+    }
+    this.newProductList = this.productList().filter(product => product.name.toLowerCase().includes(keyWord));
+  }
 }
